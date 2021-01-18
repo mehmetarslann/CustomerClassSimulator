d64b5758f8f02858458e534b86c505ffbbc39225
Mehmet <gucluhost@hotmail.com>
2021-01-18 10:28:11 +0300

    Müşteri takip sınıf simulasyonu

---
+++ CustomerManager.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MusteriTakip
+{
+    class CustomerManager
+    {
+
+        public void Add(Customer customer)
+        {
+            Console.WriteLine("Müşteri Adı : "+ customer.FirstName);
+            Console.WriteLine("Müşteri Soyadı : "+ customer.LastName);
+            Console.WriteLine("Müşteri Bakiyesi : "+ customer.Balance);
+            Console.WriteLine("Kayıt Eklendi");
+        }
+
+        public void List(Customer customer)
+        {
+            Console.WriteLine("Müşteri Adı : " + customer.FirstName);
+            Console.WriteLine("Müşteri Soyadı : " + customer.LastName);
+            Console.WriteLine("Müşteri Bakiyesi : " + customer.Balance);
+            Console.WriteLine("Kayıt Listesi");
+        }
+
+        public void Delete(Customer customer)
+        {
+            Console.WriteLine("Kayıt Silindi");
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace MusteriTakip
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CustomerManager customer = new CustomerManager();
+            Customer c = new Customer();
+            c.Id = 1;
+            c.FirstName = "Mehmet";
+            c.LastName = "Arslan";
+            c.Balance = 10000;
+            customer.Add(c);
+        }
+    }
+}
